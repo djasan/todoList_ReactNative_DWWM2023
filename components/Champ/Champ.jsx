10b02b7e527aaf9202b7d2f088035d99d5bef004
djamila key <sansondjamila@orange.fr>
2023-12-21 15:57:19 +0100

    mmkv

--- components/Champ/Champ.jsx
+++ components/Champ/Champ.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { TextInput } from 'react-native'
 import TodoListContext from '../../TodoListContext'
+import StyleChamp from './StyleChamp'
 
 const Champ = () => {
   const todoListContext = React.useContext(TodoListContext)
   return (
 
     <TextInput
+    style={StyleChamp.input}
     onChangeText={(text)=>{
       todoListContext.setSaisie(text)
     }}
---
+++ components/Champ/StyleChamp.js
@@ -0,0 +1,10 @@
+export default StyleChamp = {
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  };
+  
+  
